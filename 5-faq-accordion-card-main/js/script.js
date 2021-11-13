293b8327cadb32f01a7a7fb2bfb7045635aa6aea
Xanress <ansg1996@gmail.com>
2021-11-13 17:32:10 +0100

    finalisez work on faq

--- 5-faq-accordion-card-main/js/script.js
+++ 5-faq-accordion-card-main/js/script.js
@@ -6,8 +6,9 @@ let currentBold = document.getElementsByClassName("bold");
 for(var i = 0; i < accordion.length; i++) {
     accordion[i].addEventListener("click", function() {
 
-        // Prevent having more than collasped section
+        // Prevent having more than one collasped section.
         if (currentActive !== undefined && currentActive.length > 0) {
+            // if we click on the already collasped section we want to uncollapse it.
             if (currentActive[0].previousElementSibling !== this) {
                 currentActive[0].classList.toggle("active");
                 currentBold[0].classList.toggle("bold");
@@ -19,7 +20,5 @@ for(var i = 0; i < accordion.length; i++) {
         panel.classList.toggle('active');
         this.classList.toggle('bold');
         this.firstElementChild.classList.toggle('rotation');
-        // console.log(window.getComputedStyle(this, "::after").setProperty("text-align", ));
-        // window.getComputedStyle(this, "::after").transform;
     });
 }
